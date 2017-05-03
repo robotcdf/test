82e37bad1d401382953fe0289bc656c7a1be1604
robotcdf <cdaofang@gmail.com>
2017-05-03 22:11:15 +0800

    test ok !

--- update.sh
+++ update.sh
@@ -5,8 +5,8 @@ git add $(git ls-files -o)
 git add $(git ls-files -m)
 git rm  --ignore-unmatch [--] $(git ls-files -d)
 
+echo "git status:"
+git status
 
-#MESSAGE="$($*)"
-#git commit -m $MESSAGE
 git commit -m "$*"
 git push origin master
